6a367c9b77ffa0518fa84c3b858a3ca40715fd07
Nguyen Duc Cuong <duccuong16092003@gmail.com>
2023-04-24 17:27:03 +0700

    4.Save into table with numerical (close modal when success)

--- T3_T7-9_21114721_NguyenDucCuong/JS/index.js
+++ T3_T7-9_21114721_NguyenDucCuong/JS/index.js
@@ -1,12 +1,18 @@
 function check(){
+    // khai báo biến
     let modalinput = document.getElementsByClassName("modal-input");
     let checkresult = document.getElementsByClassName("check");
     let checkbox = document.getElementsByClassName("checkbox");
+    let info = document.getElementsByClassName("info-container");
+    let info_row = document.getElementsByClassName("info-row");
+    let select = document.getElementsByClassName("form-select");
+    sum =0;
+    // regex test
     victimid = /^BN-\d{5}$/g;
     pass = /(^.{6}$)/g;
     testma =  victimid.test(modalinput[0].value);
     testpass = pass.test(modalinput[1].value);
-    sum =0;
+    // kiểm tra mã bệnh nhân
     if(modalinput[0].value != ""){
         if(testma == true)
         {
@@ -19,6 +25,7 @@ function check(){
     else{
         checkresult[0].innerHTML = "Vui lòng không để trống";
     }
+    // kiểm tra mật khẩu
     if(modalinput[1].value != ""){
         if(testpass == true){
             checkresult[1].innerHTML = "";
@@ -30,6 +37,7 @@ function check(){
     else{
         checkresult[1].innerHTML = "Vui lòng không để trống";
     }
+    // kiểm tra ngày
     if(modalinput[2].value != ""){
         if(isDateAfterToday(modalinput[2].value) == true){
             checkresult[2].innerHTML = "";
@@ -41,13 +49,20 @@ function check(){
     else{
         checkresult[2].innerHTML = "Vui lòng không để trống";
     }
+    // tính tổng tiền
     for(i=0;i<checkbox.length;i++){
         if(checkbox[i].checked){
             sum+= 500000;
         }
     }
-    console.log(sum);
+    if(testma == true && testpass == true && isDateAfterToday(modalinput[2].value) == true){
+        info[0].innerHTML += "<div class='info-row'><div class='info-item' style='width:10%'>"+info_row.length+"</div><div class='info-item' style='width:15%'>"+modalinput[0].value+"</div><div class='info-item' style='width:15%'>"+modalinput[1].value+"</div><div class='info-item' style='width:15%'>"+modalinput[2].value+"</div><div class='info-item' style='width:22%'>"+sum+"</div><div class='info-item' style='width:22%'>"+select[0].value+"</div></div>";
+        //ẩn modal sau khi thành công
+        var myModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('myModal'));
+        myModal.hide();
+    }
 }
+// check ngày
 function isDateAfterToday(date) {
     return new Date(date) > new Date(new Date().toDateString());
 }
